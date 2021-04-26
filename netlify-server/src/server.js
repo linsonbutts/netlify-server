f1eceb8cd68ae15739e1a7590c98556a426f0f1c
unknown <linson.butts@students.cau.edu>
2021-04-26 01:23:37 -0400

    Please work cors

--- netlify-server/src/server.js
+++ netlify-server/src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const serverless = require('serverless-http')
+const cors = require('cors');
 const app = express()
 
 const router = express.Router();
 app.use(express.json());
+app.use(cors());
 
 app.use(express.urlencoded({
   extended: true
