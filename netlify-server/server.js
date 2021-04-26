e39673b5f49c851fcadc1a37f1adc512980fd12d
unknown <linson.butts@students.cau.edu>
2021-04-26 13:35:51 -0400

    ok

--- netlify-server/server.js
+++ netlify-server/server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const cors = require('cors')
 
-var corsOptions = {
-    origin: "http://localhost:3000/",
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-app.use(cors(corsOptions))
+
+app.use(cors())
 const app = express()
 
 let port = process.env.PORT || 8080
