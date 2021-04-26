9d61db5b8b014a45f54a2fcb97ba08ede60b61b1
unknown <linson.butts@students.cau.edu>
2021-04-26 14:29:56 -0400

    ok

--- netlify-server/server.js
+++ netlify-server/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+
 const app = express()
 
 let port = process.env.PORT || 8080
