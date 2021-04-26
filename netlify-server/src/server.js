53667f9b41b60340b501cc08938eb6431d1bdc51
unknown <linson.butts@students.cau.edu>
2021-04-26 01:59:13 -0400

    Please work cors

--- netlify-server/src/server.js
+++ netlify-server/src/server.js
@@ -7,6 +7,11 @@ const router = express.Router();
 app.use(express.json());
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://mystifying-jennings-b51abd.netlify.app/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.urlencoded({
   extended: true
 }));
