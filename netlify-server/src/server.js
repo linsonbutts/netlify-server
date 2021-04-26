96cb4165f3ab13bf1381605d117a76b5514e5873
unknown <linson.butts@students.cau.edu>
2021-04-25 23:54:57 -0400

    please work

---
+++ netlify-server/src/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const serverless = require('serverless-http')
+const app = express()
+
+const router = express.Router();
+app.use(express.json());
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+
+let buildingCode = "WDF";
+
+router.get('/nearest',function(req,res){
+    if(buildingCode == 'WDF'){
+        res.send('WDF')
+    }
+    else{
+        res.send('ARN')
+    }
+})
+router.post('/nearest',function(req,res){
+        if(req.body.ButtonPressA == 1){
+            buildingCode = 'ARN'
+            console.log(req.body)
+            res.send('ARN')
+        }
+        if(req.body.ButtonPressB == 1){
+            buildingCode = 'WDF'
+            console.log(req.body)
+            res.send('WDF')
+        }
+        
+    })
+
+    app.use('/.netlify/functions/server',router)
+    module.exports.handler = serverless(app)
